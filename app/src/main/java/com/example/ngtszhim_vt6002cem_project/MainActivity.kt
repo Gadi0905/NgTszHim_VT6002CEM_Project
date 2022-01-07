9c1f964def2a19d3a406744ae419c5e10e0f6b70
Gadi <gadi0905@gmail.com>
2022-01-07 20:48:48 +0800

    first commit

---
+++ app/src/main/java/com/example/ngtszhim_vt6002cem_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ngtszhim_vt6002cem_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
