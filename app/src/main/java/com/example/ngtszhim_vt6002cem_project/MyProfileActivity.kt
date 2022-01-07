bdc3f0df39a36b5e7862fe1c98fd8bffb1507ca5
Gadi <gadi0905@gmail.com>
2022-01-07 22:48:29 +0800

    added readUserData function

--- app/src/main/java/com/example/ngtszhim_vt6002cem_project/MyProfileActivity.kt
+++ app/src/main/java/com/example/ngtszhim_vt6002cem_project/MyProfileActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MyProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +14,12 @@ class MyProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_my_profile)
 
         // variable definition
+        val tvUserId = findViewById<TextView>(R.id.tvUserId)
+        val tvUserEmail = findViewById<TextView>(R.id.tvUserEmail)
         val btnMyProfileMain = findViewById<Button>(R.id.btnMyProfileMain)
         val btnLogout = findViewById<Button>(R.id.btnLogout)
+
+        var (userId, email) = readUserData(tvUserId, tvUserEmail)
 
         // when btnMyProfileMain is pressed, jump to MyProfileActivity screen
         btnMyProfileMain.setOnClickListener {
@@ -28,5 +34,24 @@ class MyProfileActivity : AppCompatActivity() {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
+    }
+
+    private fun readUserData(tvUserId: TextView, tvUserEmail: TextView): Pair<String, String> {
+        var userId = ""
+        var email = ""
+        val db = FirebaseFirestore.getInstance()
+        db.collection("users")
+            .get().addOnCompleteListener {
+                if (it.isSuccessful) {
+                    for (document in it.result!!) {
+                        userId = document.data.getValue("user_id").toString()
+                        email = document.data.getValue("email").toString()
+                    }
+                } else {
+                }
+                tvUserId.text = "User ID: $userId"
+                tvUserEmail.text = "Email: $email"
+            }
+        return Pair(userId, email)
     }
 }
