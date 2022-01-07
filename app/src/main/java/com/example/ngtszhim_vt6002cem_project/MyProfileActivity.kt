8507e7385b4e5891a209f7905002b236e799e305
Gadi <gadi0905@gmail.com>
2022-01-07 22:28:12 +0800

    complete the logout function

--- app/src/main/java/com/example/ngtszhim_vt6002cem_project/MyProfileActivity.kt
+++ app/src/main/java/com/example/ngtszhim_vt6002cem_project/MyProfileActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class MyProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +13,20 @@ class MyProfileActivity : AppCompatActivity() {
 
         // variable definition
         val btnMyProfileMain = findViewById<Button>(R.id.btnMyProfileMain)
+        val btnLogout = findViewById<Button>(R.id.btnLogout)
 
         // when btnMyProfileMain is pressed, jump to MyProfileActivity screen
         btnMyProfileMain.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        // when btnLogout is pressed
+        btnLogout.setOnClickListener {
+            // logout the app
+            FirebaseAuth.getInstance().signOut()
+            // jump to LoginActivity screen
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
         }
     }
 }
