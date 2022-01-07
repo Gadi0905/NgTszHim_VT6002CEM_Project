98ea931dce5677716fca71f2ec67e17d15e4d03b
Gadi <gadi0905@gmail.com>
2022-01-07 22:07:20 +0800

    added function to button, jump to main page and my profile page

--- app/src/main/java/com/example/ngtszhim_vt6002cem_project/MainActivity.kt
+++ app/src/main/java/com/example/ngtszhim_vt6002cem_project/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.ngtszhim_vt6002cem_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // variable definition
+        val btnMainMyProfile = findViewById<Button>(R.id.btnMainMyProfile)
+
+        // when btnMainMyProfile is pressed, jump to MyProfileActivity screen
+        btnMainMyProfile.setOnClickListener {
+            startActivity(Intent(this, MyProfileActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/ngtszhim_vt6002cem_project/MyProfileActivity.kt
+++ app/src/main/java/com/example/ngtszhim_vt6002cem_project/MyProfileActivity.kt
@@ -1,11 +1,21 @@
 package com.example.ngtszhim_vt6002cem_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MyProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
+
+        // variable definition
+        val btnMyProfileMain = findViewById<Button>(R.id.btnMyProfileMain)
+
+        // when btnMainMyProfile is pressed, jump to MyProfileActivity screen
+        btnMyProfileMain.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
