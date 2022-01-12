3b6846d81260c9c92b4a9b4b42d4a34de9e594ff
Gadi <gadi0905@gmail.com>
2022-01-12 16:31:07 +0800

    extract check location function & changed implementation

--- app/src/main/java/com/example/ngtszhim_vt6002cem_project/MainActivity.kt
+++ app/src/main/java/com/example/ngtszhim_vt6002cem_project/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.ngtszhim_vt6002cem_project
 
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -9,11 +10,18 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         // variable definition
         val btnMainMyProfile = findViewById<Button>(R.id.btnMainMyProfile)
@@ -26,16 +34,20 @@ class MainActivity : AppCompatActivity() {
 
         // when btnMap is pressed, jump to MapsActivity screen
         btnMap.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                startActivity(Intent(this, MapsActivity::class.java))
-            } else {
-                Toast.makeText(
-                    this,
-                    "Please allow the permission for location in the settings",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
+            checkLocationPermission()
+        }
+    }
+
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            startActivity(Intent(this, MapsActivity::class.java))
+        } else {
+            Toast.makeText(
+                this,
+                "Please allow the permission for location in the settings",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
--- app/src/main/java/com/example/ngtszhim_vt6002cem_project/MapsActivity.kt
+++ app/src/main/java/com/example/ngtszhim_vt6002cem_project/MapsActivity.kt
@@ -2,7 +2,6 @@ package com.example.ngtszhim_vt6002cem_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -14,17 +13,17 @@ import com.example.ngtszhim_vt6002cem_project.databinding.ActivityMapsBinding
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-private lateinit var binding: ActivityMapsBinding
+    private lateinit var binding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-     binding = ActivityMapsBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+            .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
