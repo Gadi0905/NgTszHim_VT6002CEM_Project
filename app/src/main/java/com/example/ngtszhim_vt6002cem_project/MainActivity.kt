cf39506b0ac333e7d84fa971a3f13f9458710d95
Gadi <gadi0905@gmail.com>
2022-01-12 15:56:43 +0800

    added check location permission function

--- app/src/main/java/com/example/ngtszhim_vt6002cem_project/MainActivity.kt
+++ app/src/main/java/com/example/ngtszhim_vt6002cem_project/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.ngtszhim_vt6002cem_project
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +26,16 @@ class MainActivity : AppCompatActivity() {
 
         // when btnMap is pressed, jump to MapsActivity screen
         btnMap.setOnClickListener {
-            startActivity(Intent(this, MapsActivity::class.java))
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                startActivity(Intent(this, MapsActivity::class.java))
+            } else {
+                Toast.makeText(
+                    this,
+                    "Please allow the permission for location in the settings",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
