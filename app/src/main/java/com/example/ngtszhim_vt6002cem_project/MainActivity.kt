8dd5d08f8dbaff6cd00d5b678b5aff085c126937
Gadi <gadi0905@gmail.com>
2022-01-12 20:38:23 +0800

    clean code

--- app/src/main/java/com/example/ngtszhim_vt6002cem_project/MainActivity.kt
+++ app/src/main/java/com/example/ngtszhim_vt6002cem_project/MainActivity.kt
@@ -4,18 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         // variable definition
         val btnMainMyProfile = findViewById<Button>(R.id.btnMainMyProfile)
--- app/src/main/java/com/example/ngtszhim_vt6002cem_project/MapsActivity.kt
+++ app/src/main/java/com/example/ngtszhim_vt6002cem_project/MapsActivity.kt
@@ -17,6 +17,7 @@ import com.google.android.gms.location.LocationServices
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    // variable definition
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
@@ -24,6 +25,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // get fused location to var fusedLocationProviderClient
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
@@ -47,22 +49,29 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        // get last location to task
         val task = fusedLocationProviderClient.lastLocation
 
+        // check permission, if user do not have permission, request the user to get permission
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
             ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
         }
 
+        // if the task is success
         task.addOnSuccessListener {
             if (it != null) {
+                // use a toast msg to tell user current location
                 Toast.makeText(
                     this,
                     "Your Location:${it.latitude}, ${it.longitude}",
                     Toast.LENGTH_SHORT
                 ).show()
+                // put latitude and longitude to your location
                 val yourLocation = LatLng(it.latitude, it.longitude)
+                // add a marker msg "You location" to var yourLocation
                 mMap.addMarker(MarkerOptions().position(yourLocation).title("You location"))
+                // zoom the camera to current location
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(yourLocation, 16f))
             }
         }
